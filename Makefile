abc4577538d06b6ae70ebe68b3a861639378e218
Masatoshi Tsushima <utisam@gmail.com>
2017-12-11 18:21:40 +0900

    Initial commit

---
+++ Makefile
@@ -0,0 +1,54 @@
+SHELL=/bin/bash -o pipefail
+
+TARGET_DIR = dist
+SOURCE_DIR = src
+SOURCES = $(shell find $(SOURCE_DIR) -type f -name '*.js')
+LIB_DIR = lib
+LIB_SOURCES = $(shell find lib -type f -name '*.js' -not -name '*.test.js')
+TARGETS = $(patsubst $(SOURCE_DIR)/%,$(TARGET_DIR)/%,$(SOURCES))
+
+.DELETE_ON_ERROR:
+
+# Delete the default suffixes for old-fashioned suffix rules
+.SUFFIXES:
+
+.PHONY: all test sources lint clean
+
+all: lint $(TARGETS) test sources
+
+lint: $(SOURCES) $(LIB_SOURCES)
+	npm run lint -- $(SOURCE_DIR) $(LIB_DIR)
+
+CLOSURE_COMPILER = closure-compiler/compiler.jar
+CLOSURE_COMPILATION_LEVEL = ADVANCED_OPTIMIZATIONS
+#CLOSURE_COMPILATION_LEVEL = SIMPLE_OPTIMIZATIONS
+#CLOSURE_COMPILATION_LEVEL = WHITESPACE_ONLY
+
+$(TARGET_DIR)/%.js: $(SOURCE_DIR)/%.js $(LIB_SOURCES)
+	-mkdir -p $(dir $@)
+	$(eval SOURCE_MAP_RELATIVE := $(shell echo "$<" | sed -e 's:\([^/]*\):..:g' -e 's:../..$$::g'))
+	java -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none -Xms1024m -Xmx1024m -jar $(CLOSURE_COMPILER) \
+		--language_in=ECMASCRIPT6_STRICT \
+		--language_out=ECMASCRIPT5_STRICT \
+		--process_common_js_modules \
+		--rewrite_polyfills=false \
+		--output_wrapper='(()=>{%output%})();' \
+		--compilation_level=$(CLOSURE_COMPILATION_LEVEL) \
+		--jscomp_off=checkVars \
+		--module_resolution=BROWSER \
+		--create_source_map "$@.map" \
+		--source_map_location_mapping "src|$(SOURCE_MAP_RELATIVE)src" \
+		--source_map_location_mapping "lib|$(SOURCE_MAP_RELATIVE)lib" \
+		--js_output_file $@ \
+		$^
+	echo "//# sourceMappingURL=$(notdir $@).map" >> $@
+
+test: $(TARGETS)
+	npm run test
+
+sources: $(SOURCES) $(LIB_SOURCES)
+	rsync -avr --delete $(SOURCE_DIR)/ $(TARGET_DIR)/$(SOURCE_DIR)/
+	rsync -avr --delete $(LIB_DIR)/ $(TARGET_DIR)/$(LIB_DIR)/
+
+clean:
+	-rm -rf $(TARGET_DIR)
---
+++ src/orgui-js.js
@@ -0,0 +1,39 @@
+window['orgUi'] = window['orgUi'] || {};
+const rootNS = window['orgUi'];
+
+import Modifiers, { setFlag } from '../lib/modifiers.js';
+rootNS['Modifiers'] = {
+  'HAZARDING': Modifiers.HAZARDING,
+};
+rootNS['Modifiers']['setFlag'] = setFlag;
+
+
+rootNS['components'] = rootNS['components'] || {};
+const componentsNS = rootNS['components'];
+
+import FieldGroup from '../lib/components/fieldGroup.js';
+componentsNS['FieldGroup'] = FieldGroup;
+componentsNS['FieldGroup']['prototype']['show'] = FieldGroup.prototype.show;
+componentsNS['FieldGroup']['prototype']['getChild'] = FieldGroup.prototype.getChild;
+componentsNS['FieldGroup']['prototype']['getValue'] = FieldGroup.prototype.getValue;
+componentsNS['FieldGroup']['prototype']['setValue'] = FieldGroup.prototype.setValue;
+componentsNS['FieldGroup']['prototype']['setHazarding'] = FieldGroup.prototype.setHazarding;
+componentsNS['FieldGroup']['ELEMENT_CLASSES'] = {
+  LABEL: FieldGroup.ELEMENT_CLASSES.LABEL,
+  CHILD: FieldGroup.ELEMENT_CLASSES.CHILD,
+  HELP_TEXT: FieldGroup.ELEMENT_CLASSES.HELP_TEXT,
+};
+componentsNS['FieldGroup']['Builder'] = FieldGroup.Builder;
+componentsNS['FieldGroup']['Builder']['prototype']['withDecorator'] = FieldGroup.Builder.prototype.withDecorator;
+componentsNS['FieldGroup']['Builder']['prototype']['withUnhazardingOn'] = FieldGroup.Builder.prototype.withUnhazardingOn;
+componentsNS['FieldGroup']['from'] = FieldGroup.from;
+
+import Switch from '../lib/components/switch.js';
+componentsNS['Switch'] = Switch;
+componentsNS['Switch']['ELEMENT_CLASSES'] = {
+  INPUT: Switch.ELEMENT_CLASSES.INPUT,
+  SLIDER: Switch.ELEMENT_CLASSES.SLIDER,
+};
+componentsNS['Switch']['Builder'] = Switch.Builder;
+componentsNS['Switch']['Builder']['prototype']['build'] = Switch.Builder.prototype.build;
+componentsNS['Switch']['from'] = Switch.from;
